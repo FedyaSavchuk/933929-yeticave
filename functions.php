c952dee6849efb77520f35a523295a1312aa2a69
FedyaSavchuk <fedyasavchuk@gmail.com>
2019-02-10 17:22:02 +0300

    #7 Шаблонизация проекта

---
+++ functions.php
@@ -0,0 +1,22 @@
+<?php
+function include_template($name, $data) {
+    $name = 'templates/' . $name;
+    $result = '';
+
+    if (!is_readable($name)) {
+        return $result;
+    }
+
+    ob_start();
+    extract($data);
+    require $name;
+
+    $result = ob_get_clean();
+
+    return $result;
+}
+
+function price($num) {
+    return number_format(ceil($num), 0, '.', ' ');
+}
+?>
