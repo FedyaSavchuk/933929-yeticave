de272926f305aa03b4d6a8babeda73e43a5c846d
FedyaSavchuk <fedyasavchuk@gmail.com>
2019-03-04 19:27:27 +0300

    #9 Создание схемы БД личного проекта
    Создал таблицы
    Написал индексы
    Добавил связи

--- schema.sql
+++ schema.sql
@@ -1,34 +1,70 @@
+-- Создаю таблицы
 CREATE TABLE categories (
-    id                  INT AUTO_INCREMENT PRIMARY KEY,
-    category            CHAR(128)
+    category_id         INT AUTO_INCREMENT PRIMARY KEY,
+    category            CHAR(128) UNIQUE
 );
 
 
 CREATE TABLE lot (
-    id                  INT AUTO_INCREMENT PRIMARY KEY,
+    lot_id              INT AUTO_INCREMENT PRIMARY KEY,
     date_start          DATETIME,
     lot_name            CHAR(255),
     lot_description     TEXT,
     lot_img             CHAR(255),
     first_price         INT,
     date_finish         DATETIME,
-    bid_step            INT
+    bid_step            INT,
+
+    user_id             INT,
+    category_id         INT,
+    winner_id           INT
 );
 
 
 CREATE TABLE bid (
-    id                  INT AUTO_INCREMENT PRIMARY KEY,
+    bid_id              INT AUTO_INCREMENT PRIMARY KEY,
     bid_date            DATETIME,
-    sum                 INT
+    sum                 INT,
+
+    user_id             INT,
+    lot_id              INT
 );
 
 
 CREATE TABLE user (
-    id                  INT AUTO_INCREMENT PRIMARY KEY,
+    user_id             INT AUTO_INCREMENT PRIMARY KEY, --Внешний ключ для creator_id
     registration_date   DATETIME,
     email               CHAR(255) UNIQUE,
-    user_name           CHAR(255) UNIQUE,
+    user_name           CHAR(255),
     password            CHAR(64),
     avatar              CHAR(255),
-    contact             CHAR(255)
+    contact             CHAR(255) UNIQUE
+
+    lot_id              INT,
+    bid_id              INT
 );
+
+
+-- Пишу индексы для часто искомых столбцов
+CREATE INDEX date_start ON lot(date_start);
+CREATE INDEX lot_name ON lot(lot_name);
+CREATE INDEX date_finish ON lot(date_finish);
+
+CREATE INDEX sum ON bid(sum);
+
+
+-- Делаю связи
+ALTER TABLE lot
+ADD FOREIGN KEY (user_id) REFERENCES user(user_id);
+ALTER TABLE lot
+ADD FOREIGN KEY (category_id) REFERENCES user(category_id);
+
+ALTER TABLE bid
+ADD FOREIGN KEY (user_id) REFERENCES user(user_id);
+ALTER TABLE bid
+ADD FOREIGN KEY (lot_id) REFERENCES user(lot_id);
+
+ALTER TABLE user
+ADD FOREIGN KEY (bid_id) REFERENCES user(bid_id);
+ALTER TABLE user
+ADD FOREIGN KEY (lot_id) REFERENCES user(lot_id);
